b5b67b6deebc286c60e89583806d4e4cf2df7041
Isaac Russell <24508439+sisaacrussell@users.noreply.github.com>
2024-01-11 22:05:51 -0600

    Update woocommerce-order-navigation.php
    Added github updater header

--- woocommerce-order-navigation.php
+++ woocommerce-order-navigation.php
@@ -19,6 +19,7 @@
  * Version:           1.0.0
  * Author:            Sprucely Designed
  * Author URI:        https://www.sprucely.net/
+ * GitHub Plugin URI: sprucely-designed/woocommerce-order-navigation
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       woocommerce-order-navigation
